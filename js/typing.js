699e9f4eb25b3cd41f4159d85dde483f9cfd482c
Emerson Pourghaed <ampourgh@gmail.com>
2020-07-18 00:32:15 -0500

    refactor subtitle animation

--- js/typing.js
+++ js/typing.js
@@ -5,7 +5,7 @@ var duration = 0;
 console.log(duration);
 
 var first = "EMERSON";
-var firstArray = first.split('');
+var firstArray = first.split(' ');
 
 
 // Spell out first and last name individually
@@ -17,16 +17,24 @@ for (i = 0; i < firstArray.length; i++) {
 
 console.log(duration);
 
-function lastName(duration, deleteText) {
-  var last = "POURGHAED";
-  var lastArray = last.split('');
+setTimeout('document.getElementById("last-name").innerHTML = "_";', duration);
+
+function animateWords(word, duration, deleteText) {
+  var lastArray = word.split('');
 
   duration += 1000;
 
   for (i = 0; i < lastArray.length; i++) {
+    
     console.log(duration);
     duration += 125;
-    setTimeout('document.getElementById("last-name").innerHTML += "' + lastArray[i] + '";', duration);
+
+    if (i == 0) {
+      setTimeout('document.getElementById("last-name").innerHTML = "' + lastArray[i] + '";', duration);
+    } else {
+      setTimeout('document.getElementById("last-name").innerHTML += "' + lastArray[i] + '";', duration);
+    }
+    
   }
 
   if (deleteText == true) {
@@ -36,55 +44,35 @@ function lastName(duration, deleteText) {
     for (i = i -1; i !== -1; i--) {
       console.log(duration);
       duration += 125;
-      lastArray.pop()
-      removeLast = lastArray.join('')
-      setTimeout('document.getElementById("last-name").innerHTML = "' + removeLast + '";', duration);
+      lastArray.pop();
+      removeLast = lastArray.join('');
+
+      if (i > 0) {
+        setTimeout('document.getElementById("last-name").innerHTML = "' + removeLast + '";', duration);
+      } else {
+        setTimeout('document.getElementById("last-name").innerHTML = "_";', duration);
+      }
+
     }
   }
 }
 
-function webDev(duration, deleteText) {
-  var web = "WEB DEVELOPER";
-  var webArray = web.split('');
-
-  duration += 1000;
-
-  for (i = 0; i < webArray.length; i++) {
-    console.log(duration);
-    duration += 125;
-    setTimeout('document.getElementById("last-name").innerHTML += "' + webArray[i] + '";', duration);
-  }
-
-
-  if (deleteText == true) {
-
-    duration += 1000;
-
-    for (i = i -1; i !== -1; i--) {
-      console.log(duration);
-      duration += 125;
-      webArray.pop()
-      removeLast = webArray.join('')
-      setTimeout('document.getElementById("last-name").innerHTML = "' + removeLast + '";', duration);
-    }
-  }
-}
 
 var deleteText = true;
 
 duration = 1000;
 console.log(duration);
-lastName(duration, deleteText);
+animateWords("POURGHAED", duration, deleteText);
 
 console.log(duration);
 duration = 6000;
-webDev(duration, deleteText);
+animateWords("WEB_DEVELOPER", duration, deleteText);
 
 var deleteText = false;
 
 console.log(duration);
 duration = 12000;
-lastName(duration, deleteText);
+animateWords("POURGHAED", duration, deleteText);
 
 /*
 // for when name of site is Applux instead of just Emerson
