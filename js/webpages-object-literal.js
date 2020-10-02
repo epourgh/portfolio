344c434c04dcbc3466dc55574cfa9de9ccbaea2b
Emerson Pourghaed <ampourgh@gmail.com>
2020-10-02 16:38:59 -0500

    refactor web object literal
    allow more flexibility to content displayed

--- js/webpages-object-literal.js
+++ js/webpages-object-literal.js
@@ -1,178 +1,175 @@
-const webObjectLiteral = [{
+const webObjectLiteral = [
+    {
         title: 'USA Auto Seat Covers',
-        image: 'usa-auto.PNG',
-        demo: 'http://usaautoseatcover.com/',
-        github: 'https://github.com/epourgh/Car-Seat-eBay-Catalog',
+        type: 'normal',
+        images: ['usa-auto.PNG'],
+        links: [{
+            demo: 'http://usaautoseatcover.com/',
+            github: 'https://github.com/epourgh/Car-Seat-eBay-Catalog'
+        }],
         stack: [
-            'PHP',
-            'MySQL',
-            'Heroku',
-            'HTML5',
-            'CSS3',
-            'Bootstrap'
-        ],
-        stackColor: [
-            'stack-backend',
-            'stack-db',
-            'stack-deployment',
-            'stack-html',
-            'stack-css',
-            'stack-css'
+            {name: 'PHP', color: 'stack-backend'},
+            {name: 'MySQL', color: 'stack-db'},
+            {name: 'Heroku', color: 'stack-deployment'},
+            {name: 'HTML5', color: 'stack-html'},
+            {name: 'CSS3', color: 'stack-css'},
+            {name: 'Bootstrap', color: 'color: stack-css'}
         ],
         content: [
-            'eBay catalog placed into MySQL workbench, displayed onto webpage.',
-            'PHP used to include building blocks of webpage, including how items are displayed without manually editing HTML.'
+            {
+                description: [
+                    'eBay catalog placed into MySQL workbench, displayed onto webpage.',
+                    'PHP used to include building blocks of webpage, including how items are displayed without manually editing HTML.'
+                ]
+            }
         ]
     },
     {
         title: 'Item Catalog',
-        image: 'catalog.PNG',
-        demo: 'http://ec2-52-14-27-203.us-east-2.compute.amazonaws.com/catalog/',
-        github: 'https://github.com/epourgh/Item-Catalog',
+        type: 'normal',
+        images: ['catalog.PNG'],
+        links: [{
+            demo: 'http://ec2-52-14-27-203.us-east-2.compute.amazonaws.com/catalog/',
+            github: 'https://github.com/epourgh/Item-Catalog'
+        }],
         stack: [
-            'Python',
-            'Flask',
-            'AWS',
-            'Apache',
-            'Google OAUTH API',
-            'HTML5',
-            'CSS3'
-        ],
-        stackColor: [
-            'stack-backend',
-            'stack-backend',
-            'stack-deployment',
-            'stack-deployment',
-            'stack-api',
-            'stack-html',
-            'stack-css'
+            {name: 'Python', color: 'stack-backend'},
+            {name: 'Flask', color: 'stack-backend'},
+            {name: 'AWS', color: 'stack-deployment'},
+            {name: 'Apache', color: 'stack-deployment'},
+            {name: 'Google OAUTH API', color: 'stack-api'},
+            {name: 'HTML5', color: 'stack-html'},
+            {name: 'CSS3', color: 'stack-css'}
         ],
         content: [
-            'Backend catalog site with Google account login functionality. Ensures high security authentication.',
-            'User can create categories and a list of items within, with Google icons and names indicating which user created the content.'
+            {
+                description: [
+                    'Backend catalog site with Google account login functionality. Ensures high security authentication.',
+                    'User can create categories and a list of items within, with Google icons and names indicating which user created the content.'
+                ]
+            }
         ]
     },
     {
         title: 'Restaurant',
-        image: 'restaurant-site.png',
-        demo: 'https://epourgh.github.io/Restaurant/',
-        github: 'https://github.com/epourgh/Restaurant',
+        type: 'normal',
+        images: ['restaurant-site.png'],
+        links: [{
+            demo: 'https://epourgh.github.io/Restaurant/',
+            github: 'https://github.com/epourgh/Restaurant'
+        }],
         stack: [
-            'JavaScript',
-            'React',
-            'JQuery',
-            'HTML5',
-            'CSS3',
-            'Bootstrap'
-        ],
-        stackColor: [
-            'stack-frontend',
-            'stack-frontend',
-            'stack-frontend',
-            'stack-html',
-            'stack-css',
-            'stack-css'
+            {name: 'JavaScript', color: 'stack-frontend'},
+            {name: 'React', color: 'stack-frontend'},
+            {name: 'JQuery', color: 'stack-frontend'},
+            {name: 'HTML5', color: 'stack-html'},
+            {name: 'CSS3', color: 'stack-css'},
+            {name: 'Bootstrap', color: 'stack-css'}
         ],
         content: [
-            'Single page display for restaurant with menus for Restaurant\'s menu, operating hours, and location.',
-            'With user first approach design in mind, simple and easy to use interface.'
+            {
+                description: [
+                    'Single page display for restaurant with menus for Restaurant\'s menu, operating hours, and location.',
+                    'With user first approach design in mind, simple and easy to use interface.'
+                ]
+            }
         ]
     },
     {
         title: 'A to Z One Day',
-        image: 'a-z.PNG',
-        demo: 'http://a-to-z-one-day.com/',
-        github: 'https://github.com/epourgh/Car-Seat-eBay-Catalog/tree/master/a-to-z',
+        type: 'normal',
+        images: ['a-z.PNG'],
+        links: [{
+            demo: 'http://a-to-z-one-day.com/',
+            github: 'https://github.com/epourgh/Car-Seat-eBay-Catalog/tree/master/a-to-z'
+        }],
         stack: [
-            'Photoshop',
-            'Illustrator',
-            'Illustrator',
-            'JQuery',
-            'HTML5',
-            'CSS3',
-            'Bootstrap'
-        ],
-        stackColor: [
-            'stack-adobe',
-            'stack-adobe',
-            'stack-adobe',
-            'stack-frontend',
-            'stack-html',
-            'stack-css',
-            'stack-css'
+            {name: 'Photoshop', color: 'stack-adobe'},
+            {name: 'Illustrator', color: 'stack-adobe'},
+            {name: 'Illustrator', color: 'stack-adobe'},
+            {name: 'JQuery', color: 'stack-frontend'},
+            {name: 'HTML5', color: 'stack-html'},
+            {name: 'CSS3', color: 'stack-css'},
+            {name: 'Bootstrap', color: 'stack-css'}
         ],
         content: [
-            'Designed logo and category images with the use of Adobe Creative Suite; Photoshop and Illustrator.',
-            'Single page navigation smooth scrolling going from section to section.'
+            {
+                description: [
+                    'Designed logo and category images with the use of Adobe Creative Suite; Photoshop and Illustrator.',
+                    'Single page navigation smooth scrolling going from section to section.'
+                ]
+            }
         ]
     },
     {
         title: 'Calendar',
-        image: 'calendar.png',
-        demo: '',
-        github: 'https://github.com/epourgh/Calendar',
+        type: 'normal',
+        images: ['calendar.png'],
+        links: [{
+            demo: '',
+            github: 'https://github.com/epourgh/Calendar'
+        }],
         stack: [
-            'PHP',
-            'JavaScript',
-            'CSS'
-        ],
-        stackColor: [
-            'stack-backend',
-            'stack-frontend',
-            'stack-css'
+            {name: 'PHP', color: 'stack-backend'},
+            {name: 'JavaScript', color: 'stack-frontend'},
+            {name: 'CSS', color: 'stack-css'}
         ],
         content: [
-            'Calendar for typing what you have done during the day, or are able to be around for.',
-            'Design with CSS, easy to read and sleek design.',
-            'PHP used to get calendar data and loop data into the template.'
+            {
+                description: [
+                    'Calendar for typing what you have done during the day, or are able to be around for.',
+                    'Design with CSS, easy to read and sleek design.',
+                    'PHP used to get calendar data and loop data into the template.'
+                ]
+            }
         ]
     },
     {
         title: 'Houston Map Filter',
-        image: 'google-maps.png',
-        demo: 'https://epourgh.github.io/Neighborhood-Map/v1/',
-        github: 'https://github.com/epourgh/Neighborhood-Map/v1',
+        type: 'normal',
+        images: ['google-maps.png'],
+        links: [{
+            demo: 'https://epourgh.github.io/Neighborhood-Map/v1/',
+            github: 'https://github.com/epourgh/Neighborhood-Map/v1'
+        }],
         stack: [
-            'JavaScript',
-            'KnockOut.js',
-            'JQuery',
-            'Google Maps API',
-            'Wikipedia API',
-            'CSS',
-            'HTML'
-        ],
-        stackColor: [
-            'stack-frontend',
-            'stack-frontend',
-            'stack-frontend',
-            'stack-api',
-            'stack-api',
-            'stack-css',
-            'stack-html'
+            {name: 'JavaScript', color: 'stack-frontend'},
+            {name: 'KnockOut.js', color: 'stack-frontend'},
+            {name: 'JQuery', color: 'stack-frontend'},
+            {name: 'Google Maps API', color: 'stack-api'},
+            {name: 'Wikipedia API', color: 'stack-api'},
+            {name: 'CSS', color: 'stack-css'},
+            {name: 'HTML', color: 'stack-html'}
         ],
         content: [
-            'Using Google Maps, placed map markers around the city of Houston of specific locations recommended for locals and tourists to visit.',
-            'Left of map is a filter functionality of the locations, and each marker displays Wikipedia information when clicked.'
+            {
+                description: [
+                    'Using Google Maps, placed map markers around the city of Houston of specific locations recommended for locals and tourists to visit.',
+                    'Left of map is a filter functionality of the locations, and each marker displays Wikipedia information when clicked.'
+                ]
+            }   
         ]
     },
     {
         title: '',
-        image: '',
+        type: 'showcase',
+        images: [''],
+        links: [{
+            demo: '',
+            github: ''
+        }],
         demo: '',
         github: '',
         stack: [
-            '',
-            '',
-            '',
-            '',
-            ''
+            {name: '', color: ''}
         ],
         content: [
-            '',
-            '',
-            '',
-            '',
-            ''
+            {
+                description: [
+                    '',
+                    ''
+                ]
+            }
         ]
     }
 ];
