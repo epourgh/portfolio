4186c7aa8ea3c4a537cd26efeca1e70c497469f8
epourgh <epourgh@gmail.com>
2021-01-18 08:24:56 -0600

    mobile navbar

--- js/nav.js
+++ js/nav.js
@@ -2,15 +2,18 @@
 mobileNavToggle = () => {
     
     let nav = document.getElementById("left-nav");
+    let rightBody = document.getElementById("mobile-right-body")
     let hamburger = document.getElementById("mobile-nav-toggle-hamburger");
 
-    if(nav.style.display == "none") {
+    if(nav.style.left == "-75vw") {
         console.log('was none')
-        nav.style.display = "block";
+        nav.style.left = "0vw";
+        rightBody.style.left = "75vw";
         hamburger.style.display = "none";
     } else {
         hamburger.style.display = "block";
-        nav.style.display = "none";
+        nav.style.left = "-75vw";
+        rightBody.style.left = "0vw";
     }
 
 }
